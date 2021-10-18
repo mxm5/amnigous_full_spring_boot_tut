8c1a140cd14bf469dfea9a9ce1c654fb09836977
CEO <vaniladra@gmail.com>
2021-10-18 18:42:57 +0330

    reading the server from js in console and then mapping them in a simple list

--- src/js/src/App.js
+++ src/js/src/App.js
@@ -1,12 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
+import {getAllStudents} from "./client";
+import {Component} from "react";
 
-function App() {
-  return (
-    <h1>
-      hellllo
-    </h1>
-  );
+
+class App extends Component {
+
+    state = {
+        students: []
+    }
+
+
+    componentDidMount() {
+        this.fetchStudents();
+    }
+
+    fetchStudents = () => {
+        getAllStudents().then(res => res.json().then(
+            students => {
+                console.log(students);
+                this.setState({students});
+                console.log(this.state)
+            }
+        ));
+    }
+
+
+    render() {
+        const {students} = this.state;
+
+        if (students && students.length) {
+
+            return students.map((value, index) => {
+                return (
+                    // , value.lastName, value.email, value.gender, value.studentId
+                    <div key={index}>
+                        <h3>
+                            {value.studentId}
+                        </h3>
+                        <p>
+                            {value.firstName}
+                        </p>
+                        <p>
+                            {value.lastName}
+                        </p>
+                        <p>
+                            {value.gender}
+                        </p>
+                        <p>
+                            {value.email}
+                        </p>
+                    </div>
+                );
+            });
+        } else
+
+            return (
+                <h1>
+                    no students found
+                </h1>
+            );
+
+    }
 }
 
 export default App;
